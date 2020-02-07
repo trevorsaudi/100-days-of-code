aca3c017ad8f658abddfaad83b3f09c57432d5e5
Trevor Saudi <38811409+trevorsaudi@users.noreply.github.com>
2020-02-07 11:14:12 +0300

    Material react CRUD application

---
+++ day61/src/Components/Exercises/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import LeftPane from './LeftPane'
+import RightPane from './RightPane'
+
+const styles = {
+	Paper :{ padding: 20, marginTop: 10, marginBottom: 10 }
+}
+
+export default props =>
+<Grid container>
+
+	<Grid item sm>
+	
+	<LeftPane styles ={styles} />
+
+	</Grid >
+
+	<Grid item sm>
+	<RightPane styles ={styles} />
+	</Grid >
+
+</Grid>
