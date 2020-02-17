033ce87c2f71ab0200836a06c338015b0f68c22f
Trevor Saudi <38811409+trevorsaudi@users.noreply.github.com>
2020-02-17 21:05:25 +0300

    Material react CRUD application

---
+++ day 63/src/Components/App.js
@@ -0,0 +1,78 @@
+import React , {Component, Fragment} from 'react'
+import {Header, Footer} from './Layouts'
+import Exercises from './Exercises'
+import { muscles, exercises } from '../store.js'
+
+
+class App extends Component{
+
+	state = {
+		exercises,
+		exercise:{}
+
+	}
+
+getExercisesByMuscles(){
+	return Object.entries( this.state.exercises.reduce((exercises, exercise) =>{
+			const { muscles } = exercise
+
+			exercises[muscles] = exercises[muscles]
+			?[...exercises[muscles], exercise]
+			:[exercise]
+
+			return exercises
+	},{})
+	)  
+}
+
+handleCategorySelected = category =>{
+	this.setState({
+		category
+	})
+
+}
+
+handleExerciseSelected = id =>  {
+	this.setState(({exercises}) => ({
+
+			exercise:exercises.find(ex => ex.id == id)
+	}))
+
+
+	
+	}
+
+
+
+render(){
+	const exercises = this.getExercisesByMuscles(),
+	{category, exercise} = this.state
+	return (
+	 <Fragment>
+
+			<Header />
+			<Exercises 
+
+			exercises = {exercises}
+			category = {category}
+			exercise = {exercise}
+			onSelect = {this.handleExerciseSelected}
+			
+
+			/>
+
+			<Footer 
+			category ={category}
+			muscles ={muscles}
+			onSelect={this.handleCategorySelected}
+			/>
+
+
+
+   </Fragment>
+)
+}
+
+ }
+
+ export default App
