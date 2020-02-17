8c6cf4cb93d683fabe1a4b7d3dfcbe1fe3a5a21d
Trevor Saudi <38811409+trevorsaudi@users.noreply.github.com>
2020-02-17 21:06:47 +0300

    Material React CRUD application

---
+++ day 65/Components/App.js
@@ -0,0 +1,90 @@
+import React , {Component, Fragment} from 'react'
+import {Header, Footer} from './Layouts'
+import Exercises from './Exercises'
+import { muscles, exercises } from '../store.js'
+
+
+class App extends Component{
+
+	state = {
+		exercises,
+		exercise:{}
+
+	}
+
+getExercisesByMuscles(){
+	return Object.entries( this.state.exercises.reduce((exercises, exercise) =>{
+			const { muscles } = exercise
+
+			exercises[muscles] = exercises[muscles]
+			?[...exercises[muscles], exercise]
+			:[exercise]
+
+			return exercises
+	},{})
+	)  
+}
+
+handleCategorySelect = category =>{
+	this.setState({
+		category
+	})
+
+}
+
+handleExerciseSelect = id =>  {
+	this.setState( ({exercises}) => ({
+
+			exercise:exercises.find(ex => ex.id == id)
+	}))
+
+
+	
+	}
+
+handleExerciseCreate = exercise =>{
+	this.setState(({exercises}) => ({
+		exercises:[ 
+		...exercises,
+		exercise
+		]
+	}))
+
+}
+
+
+render(){
+	const exercises = this.getExercisesByMuscles(),
+	{category, exercise} = this.state
+	return (
+	 <Fragment>
+
+			<Header 
+			muscles={muscles}
+			onExerciseCreate={this.handleExerciseCreate}
+			/>
+			<Exercises 
+
+			exercises = {exercises}
+			category = {category}
+			exercise = {exercise}
+			onSelect = {this.handleExerciseSelect}
+			
+
+			/>
+
+			<Footer 
+			category ={category}
+			muscles ={muscles}
+			onSelect={this.handleCategorySelect}
+			/>
+
+
+
+   </Fragment>
+)
+}
+
+ }
+
+ export default App
---
+++ day 65/Components/Exercises/index.js
@@ -0,0 +1,86 @@
+import React , { Fragment} from 'react'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItem/ListItemSecondaryAction';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+const styles = {
+	Paper :{ padding: 20,
+			marginTop: 10, 
+			marginBottom: 10 , 
+			height: 500, 
+			overflowY:'auto'}
+}
+
+export default ({ 
+		exercises,
+		category, 
+		onSelect,
+		exercise: {
+			id, 
+			title= 'Welcome!',
+		    description='Please select exercises from the categories below'}
+	 })=>
+<Grid container>
+
+	<Grid item sm>
+	
+		<Paper style = {styles.Paper}>
+		{exercises.map(([group, exercises]) =>
+			!category || category === group
+	 ?<Fragment key={category}>
+		<Typography 
+			variant = "headline"
+			style = {{textTransform: 'capitalize'}}>
+				{group}
+			</Typography>
+	<List component="ul">
+       {exercises.map(({id, title}) =>
+       
+       
+        <ListItem 
+         key = {id}
+         button
+         onClick = {() => onSelect(id)} >
+          <ListItemText primary={title}/>
+
+          <ListItemSecondaryAction>
+          <IconButton>
+          </IconButton>
+
+
+          </ListItemSecondaryAction>
+       
+        </ListItem>
+        )}
+      
+    </List>
+    
+    </Fragment>
+:null
+			)}
+	</Paper>
+
+	</Grid>
+
+	<Grid item sm>
+		<Paper style = {styles.Paper}>
+		<Typography
+		variant="h5">
+		{title}
+		</Typography>
+		<Typography
+		variant ="subheading"
+		style={{marginTop: 20}}
+		>
+	{description}
+
+		</Typography>
+	</Paper>
+	</Grid>
+
+</Grid>
