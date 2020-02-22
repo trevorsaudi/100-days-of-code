d6606bf6157a6e4fbc5512227e44bc1c2fe22047
Trevor Saudi <38811409+trevorsaudi@users.noreply.github.com>
2020-02-22 18:40:54 +0300

    Material UI CRUD

---
+++ day 74/src/Components/Layouts/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import CreateDialog from '../Exercises/Dialog'
+
+export default ({muscles, onExerciseCreate}) =>
+
+<AppBar position="static">
+  <Toolbar>
+  <Typography variant="headline" color="inherit" style={{flex: 1}}>
+ 		Exercise Database    
+      </Typography>
+
+      <CreateDialog
+      muscles={muscles}
+      onCreate={onExerciseCreate}
+      />
+    
+  </Toolbar>
+</AppBar>
